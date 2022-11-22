df290d81ec41ea896ee5494e72b30056343c38dd
Emina <you@example.com>
2022-11-22 17:52:36 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+function inflationCalculator () {
+    let  inflationRate = document.querySelector("#inflationRate")
+    let money = document.querySelector("#money")
+    let years = document.querySelector("#years")
+
+    inflationRate = parseFloat(inflationRate.value)
+    money = parseFloat(money.value)
+    years = parseInt(years.value)
+
+    let worth = money + ( money * ( inflationRate / 100))
+
+     for(let i = 1; i  < years; i++) {
+        worth += worth * (inflationRate / 100)
+    }
+
+    worth = worth.toFixed(2)
+
+    let newElement = document.createElement("div")
+    newElement.className = "new-value"
+    newElement.innerText = "Danasnjih " + money + "$ vrijedi isto kao " + worth + "$ za " + years + " godina."
+
+    document.querySelector(".container").appendChild(newElement)
+}
